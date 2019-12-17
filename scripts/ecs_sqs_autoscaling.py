b11c371ec5704bc4fa282b5fce8c9a48ba681179
Charlie <charlie@charliemathews.com>
2019-12-16 18:28:36 -0600

    Test a bunch of edge cases and compress into lpipe-ready function

--- scripts/ecs_sqs_autoscaling.py
+++ scripts/ecs_sqs_autoscaling.py
@@ -2,81 +2,129 @@ import datetime
 from lpipe.utils import get_nested
 from aws_sso import boto3_client
 
+
 def check_status(response, code=2, keys=["ResponseMetadata", "HTTPStatusCode"]):
     status = get_nested(response, keys)
     assert status // 100 == code
     return status
 
-target_pressure = 5000
+
+def _call(_callable, *args, **kwargs):
+    try:
+        resp = _callable(*args, **kwargs)
+        check_status(resp)
+        return resp
+    except ClientError as e:
+        if e.response.get("Error", {}).get("Code") != "ResourceAlreadyExistsException":
+            raise
+
+
+def pressure(approx_num_msgs: int, desired_count: int, target_task_pressure: int):
+    if target_task_pressure == 0:
+        return 100
+    if approx_num_msgs > 0 and desired_count == 0:
+        return 200
+    try:
+        return int(((approx_num_msgs / desired_count) / target_task_pressure) * 100)
+    except ZeroDivisionError:
+        return 0
+
+
+def calculate_pressure(service_name: str, queue_name: str, target_task_pressure: int):
+    sqs = boto3_client('sqs', region_name='us-east-2')
+    ecs = boto3_client('ecs', region_name='us-east-2')
+
+    url = get_nested(
+        _call(
+            sqs.get_queue_url,
+            QueueName=queue_name
+        ),
+        ["QueueUrl"]
+    )
+    approx_num_msgs = int(get_nested(
+        _call(
+            sqs.get_queue_attributes,
+            QueueUrl=url,
+            AttributeNames=["ApproximateNumberOfMessages"]
+        ),
+        ["Attributes", "ApproximateNumberOfMessages"]
+    ))
+    desired_count = int(_call(
+        ecs.describe_services,
+        services=[service_name]
+    )["services"][0]["desiredCount"])
+
+    p = pressure(approx_num_msgs, desired_count, target_task_pressure)
+
+    print(f"Write pressure to cloudwatch: {p}")
+    #now = datetime.datetime.now()
+
+
+
 #service_name = "pypedream-orchestrator"
 #queue_name = "orchestrator-input-queue.fifo"
 service_name = "pypedream-taxonomy"
 queue_name = "taxonomy-input-queue.fifo"
+target_task_pressure = 5000
 
-client = boto3_client('sqs', region_name='us-east-2')
+calculate_pressure(service_name, queue_name, target_task_pressure)
 
-resp = client.get_queue_url(QueueName=queue_name)
-assert check_status(resp)
-url = resp["QueueUrl"]
-print(f"url: {url}")
 
-now = datetime.datetime.now()
+# print("")
+# print(f"Simulating effects of autoscaling...")
+# print(f"( ( approx_num_msgs({approx_num_msgs}) / desired_count({desired_count}) ) / target_task_pressure({target_task_pressure}) ) * 100")
+# if p == float("inf"):
+#     pass
+# elif p > 100:
+#     print("We should scale UP.")
+#     new_desired_count = desired_count + 1
+#     ideal_p = pressure(approx_num_msgs, new_desired_count, target_task_pressure)
+#     while ideal_p > 100:
+#         new_desired_count += 1
+#         ideal_p = pressure(approx_num_msgs, new_desired_count, target_task_pressure)
+# elif p < 100:
+#     print("We should scale DOWN.")
+#     new_desired_count = desired_count - 1 if desired_count > 0 else desired_count
+#     ideal_p = pressure(approx_num_msgs, new_desired_count, target_task_pressure)
+#     while ideal_p < 100:
+#         new_desired_count -= 1
+#         if new_desired_count == 0:
+#             break
+#         ideal_p = pressure(approx_num_msgs, new_desired_count, target_task_pressure)
+#
+# print(f"ideal_pressure: {ideal_p}")
+# print(f"ideal_desired_count: {new_desired_count}")
 
-resp = client.get_queue_attributes(QueueUrl=url, AttributeNames=["ApproximateNumberOfMessages"])
-assert check_status(resp)
-approx_num_msgs = resp["Attributes"]["ApproximateNumberOfMessages"]
-print(f"approx_num_msgs: {approx_num_msgs}")
 
-client = boto3_client('ecs', region_name='us-east-2')
+print("")
+print("Tests")
 
-resp = client.describe_services(services=[service_name])
-assert check_status(resp)
-try:
-    desired_count = resp["services"][0]["desiredCount"]
-except:
-    print(resp)
-    raise
-print(f"desired_count: {desired_count}")
+tests = (
+    (0, 0, 100, 0),
+    (0, 1, 100, 0),
+    (1, 0, 100, 200),
+    (1000000, 0, 100, 200),
+    (1000000, 1, 100, 1000000),
+    (1, 1, 100, 1),
+    (0, 0, 0, 100),
+    (100, 0, 0, 100),
+    (100, 1, 0, 100),
+    (100, 9, 10, 111),
+    (100, 10, 10, 100),
+    (100, 11, 10, 90),
+    (999999999, 10, 10, 999999999),
+    (1000001, 1, 10000, 10000),
+    (1000001, 10, 10000, 1000),
+    (1000001, 100, 10000, 100),
+)
 
-class ServiceScaledToZero(Exception):
-    pass
-
-def pressure(approx_num_msgs, desired_count, target_pressure):
-    if desired_count == 0:
-        raise ServiceScaledToZero()
-    return ((int(approx_num_msgs) / int(desired_count)) / target_pressure) * 100
-
-print(f"( ( approx_num_msgs({approx_num_msgs}) / desired_count({desired_count}) ) / target_pressure({target_pressure}) ) * 100")
-try:
-    p = pressure(approx_num_msgs, desired_count, target_pressure)
-except ServiceScaledToZero:
-    if approx_num_msgs > 0:
-        new_desired_count = 1
-        p = 100
-        print("ServiceScaledToZero but we have messages. We should scale UP.")
-
-print(f"pressure: {p}")
-
-if p > 100:
-    print("We should scale UP.")
-    new_desired_count = desired_count + 1
-    ideal_p = pressure(approx_num_msgs, new_desired_count, target_pressure)
-    while ideal_p > 100:
-        new_desired_count += 1
-        ideal_p = pressure(approx_num_msgs, new_desired_count, target_pressure)
-
-elif p < 100:
-    print("We should scale DOWN.")
-    new_desired_count = desired_count - 1 if desired_count > 0 else desired_count
-    ideal_p = pressure(approx_num_msgs, new_desired_count, target_pressure)
-    while ideal_p < 100:
-        if new_desired_count - 1 == 0:
-            break
-        new_desired_count -= 1
-        ideal_p = pressure(approx_num_msgs, new_desired_count, target_pressure)
-
-print(f"ideal_pressure: {ideal_p}")
-print(f"ideal_desired_count: {new_desired_count}")
+for t in tests:
+    p = pressure(t[0], t[1], t[2])
+    try:
+        assert p == t[3]
+        print(f"PASS get_pressure({t[0]}, {t[1]}, {t[2]}) == {t[3]}%")
+    except AssertionError:
+        print(f"FAIL get_pressure({t[0]}, {t[1]}, {t[2]})->{p}% != {t[3]}%")
 
 
 # client = boto3_client('cloudwatch', region_name='us-east-2')
